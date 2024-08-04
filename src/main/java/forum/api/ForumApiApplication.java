246f2e55c50969402ff0f0eb57bf80fd00bb2f2e
Julian <julian.lopezvel@gmail.com>
2024-08-03 20:00:47 -0500

    First commit, creation of Spring project

---
+++ src/main/java/forum/api/ForumApiApplication.java
@@ -0,0 +1,12 @@
+package forum.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumApiApplication.class, args);
+    }
+}
