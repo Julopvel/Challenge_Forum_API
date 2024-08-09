9de2b4d610d607d07e311156d16f40f4c0040464
Julian <julian.lopezvel@gmail.com>
2024-08-08 22:01:04 -0500

    Method findByActiveTrue to list all active topics

--- src/main/java/forum/api/domain/topic/TopicRepository.java
+++ src/main/java/forum/api/domain/topic/TopicRepository.java
@@ -1,6 +1,10 @@
 package forum.api.domain.topic;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+
+    Page<Topic> findByActiveTrue(Pageable pageable);
 }
